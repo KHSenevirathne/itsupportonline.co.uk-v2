6c232fbaaeadc5f63a12c26cdfe39a1280845ca6
KHSenevirathne <khsenevirathne@gmail.com>
2025-05-15 15:31:00 +0530

    test commit

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export const metadata = {
   title:
     "IT Support - On Demand IT Services Specialist",
   description:
-    "IT Support services UK company in London for expert technology advice to help business growth! Trusted IT specialists by over 200+ companies For over 10 years..",
+    "IT Support services UK company in London for expert technology advice to help business growth! Trusted IT specialists by over 200+ companies For over 10 years.",
 };
 export default function Home() {
   return (
